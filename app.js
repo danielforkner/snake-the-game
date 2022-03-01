e2e4d63b701824c752bc90a416a501da21f56e7b
danielforkner <daniel.forkner@gmail.com>
2022-03-01 11:40:16 -0500

    save

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
-const btn = document.querySelector('button');
-const gameArea = document.querySelector('.gameArea');
-const borders = document.querySelectorAll('.border');
-const fires = document.querySelectorAll('.fire');
-const FIRE_DELAY = 150;
-const HEIGHT = 300; // height of gameArea in px
-const WIDTH = 600; // width of gameArea in px
-const CELL_SIZE = 15; // hw of cell in px
+let elements = {
+
+}
 
 let gameState = {
     currentScore: 0,
     bossHealth: 100,
     gameStatus: 'not playing',
+    dialogueCounter: 0,
+    playerName: 'SNEK',
 }
 
-btn.addEventListener('click', () => {
-    // slitherIn(); add the .slither class. Then have the top image slide in for text.
+const dialogue = [
+    'There is a monster below',
+    'Only you can defeat it',
+    'Will you help?',
+    'Thank you great hero',
+    'Who are you?',
+    '<input type="textarea" placeholder="your name" id="nameInput" />',
+    `Thank you ${gameState.playerName}!`,
+    ];
+const dialogueBtn = document.querySelector('.dialogueBtn');
+const textArea = document.querySelector('.text');
+
+const startBtn = document.querySelector('button');
+const heroes = document.querySelector('.heroes');
+const gameArea = document.querySelector('.gameArea');
+const borders = document.querySelectorAll('.border');
+const fires = document.querySelectorAll('.fire');
+const snake = document.querySelector('.snake');
+const FIRE_DELAY = 150;
+const SNAKE_DELAY = 3000;
+const BOSS_DELAY = 3000;
+const HEIGHT = 300; // height of gameArea in px
+const WIDTH = 600; // width of gameArea in px
+const CELL_SIZE = 15; // hw of cell in px
+
+// EVENT LISTENERS
+startBtn.addEventListener('click', () => {
     createGameArea();
-    makeFire();
+    goDown();
+    enterSnake();
+    enterBoss();
+    setTimeout(() => {
+        makeFire();
+    }, SNAKE_DELAY + BOSS_DELAY);
 })
 
-// createGameArea();
+dialogueBtn.addEventListener('click', () => {
+    if (document.getElementById('nameInput')) {
+        gameState.playerName = document.getElementById('nameInput').value
+    }
+    textArea.innerHTML = dialogue[gameState.dialogueCounter]
+    gameState.dialogueCounter++;
+})
+
 
 // create gamearea
 function createGameArea() {
@@ -48,4 +82,16 @@ function makeFire() {
         borders[0].style.backgroundColor = 'brown';
         borders[1].style.backgroundColor = 'brown';
     }, 20*FIRE_DELAY)
+}
+
+function goDown() {
+    heroes.classList.add('goDown');
+}
+
+function enterSnake() {
+    snake.classList.add('slither');
+}
+
+function enterBoss() {
+
 }
